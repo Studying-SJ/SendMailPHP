41f530dd3cdb79ebb50a09858768eda8383c60c0
Samuel-Jesus <samuel.jesan2018@gmail.com>
2023-01-04 02:51:16 -0300

    Corrigindo erro na autenticação do email

--- processa_envio.php
+++ processa_envio.php
@@ -43,12 +43,12 @@
 	$mail = new PHPMailer(true);
 	try {
 			//Server settings
-			$mail->SMTPDebug = false;                      //Enable verbose debug output
+			$mail->SMTPDebug = false;                      				//Enable verbose debug output
 			$mail->isSMTP();                                            //Send using SMTP
-			$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+			$mail->Host       = 'smtp.gmail.com';                     	//Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-			$mail->Username   = 'mailtesterphp2021@gmail.com';                     //SMTP username
-			$mail->Password   = '@tester13579';                               //SMTP password
+			$mail->Username   = 'siis.online.dev@gmail.com';			//SMTP username
+			$mail->Password   = 'pmyjxwlpocqsssqf';						    //SMTP password
 			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
