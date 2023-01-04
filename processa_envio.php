4e4b3a5443da8a883104282c89c521275f6905c1
Samuel-Jesus <samuel.jesan2018@gmail.com>
2023-01-04 03:10:56 -0300

    Corrigindo vunerabilidade de senha no doc

--- processa_envio.php
+++ processa_envio.php
@@ -47,8 +47,8 @@
 			$mail->isSMTP();                                            //Send using SMTP
 			$mail->Host       = 'smtp.gmail.com';                     	//Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-			$mail->Username   = 'siis.online.dev@gmail.com';			//SMTP username
-			$mail->Password   = 'pmyjxwlpocqsssqf';						    //SMTP password
+			$mail->Username   = 'conta_gmail@gmail.com';			//SMTP username
+			$mail->Password   = 'crie_uma_senha_para_app_no_gmail';						    //SMTP password
 			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
